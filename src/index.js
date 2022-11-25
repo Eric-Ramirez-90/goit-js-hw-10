bd404045b3ff180d4bf4e833987f006ace367391
Eric Ramirez <ramirez1990rer@gmail.com>
2022-11-25 03:51:41 +0200

    Completed HW 10

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 function onSearch(evt) {
   const inputValue = evt.target.value.trim();
   if (!inputValue) {
+    clearCountryList();
+    clearCountryInfo();
     return;
   }
 
@@ -25,29 +27,66 @@ function onSearch(evt) {
         Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
+        clearCountryList();
+        clearCountryInfo();
+        return;
+      }
+      if (data.length === 1) {
+        refs.countryInfo.innerHTML = createMarkupCountryInfo(data);
+        clearCountryList();
         return;
       }
 
-      if (data.length < 2) {
-        refs.countryInfo.insertAdjacentHTML('beforeend', createMarkup(data));
+      if (data.length >= 2 || data.length < 10) {
+        refs.countryList.innerHTML = createMarkupCountryList(data);
+        clearCountryInfo();
+        return;
       }
     })
     .catch(error => {
       Notify.failure('Oops, there is no country with that name.');
+      clearCountryList();
+      clearCountryInfo();
     });
 }
 
-function createMarkup(arr) {
+function createMarkupCountryInfo(arr) {
   return arr
     .map(
       ({ name, flags, languages, population, capital }) =>
-        `<li class="js-item">
-      <img class="js-item__img" src="${flags.svg}" alt="${name}" width="50" />
-      <h2>${name}</h2>
-      <p><span>Capital:</span> ${capital}</p>
-      <p><span>Population:</span> ${population}</p>
-      <p><span>Languages:</span> ${languages}</p>
-    </li>`
+        `<div class="js-card">
+        <div class="js-card-top">
+      <img class="js-card-img" src="${flags.svg}" alt="${name.official}" />
+      <h2 class="js-card-title">${name.common}</h2>
+      </div>
+      <div class="js-card-info">
+      <p class="js-card-text"><span class="js-card-span">Capital:</span> ${capital}</p>
+      <p class="js-card-text"><span class="js-card-span">Population:</span> ${population}</p>
+      <p class="js-card-text"><span class="js-card-span">Languages:</span> ${Object.values(
+        languages
+      )}</p>
+    </div>
+    </div>`
     )
     .join('');
+}
+
+function createMarkupCountryList(arr) {
+  return arr
+    .map(
+      ({ name, flags }) =>
+        `<li class="country-list__item">
+        <img class="js-item__img" src="${flags.svg}" alt="${name.official}">
+        <h2 class="js-title">${name.common}</h2>
+      </li>`
+    )
+    .join('');
+}
+
+function clearCountryList() {
+  refs.countryList.innerHTML = '';
+}
+
+function clearCountryInfo() {
+  refs.countryInfo.innerHTML = '';
 }
