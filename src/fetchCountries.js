4e3f4694607a21a1abe0fc4066346e2880b3d047
Eric Ramirez <ramirez1990rer@gmail.com>
2022-11-30 14:41:48 +0200

    added async

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,10 @@
-export function fetchCountries(name) {
+export async function fetchCountries(name) {
   const BASE_URL = 'https://restcountries.com/v3.1/name';
-  return fetch(
+  const resp = await fetch(
     `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
-  ).then(resp => {
-    if (!resp.ok) {
-      throw new Error();
-    }
-    return resp.json();
-  });
+  );
+  if (!resp.ok) {
+    throw new Error();
+  }
+  return await resp.json();
 }
